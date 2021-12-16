c9d72f2f7fbe9daa681b261250213af59ecdc711
Yonghao Shu <123shuyonghao@gmail.com>
2021-12-15 21:23:17 -0800

    Bean factory for Dao with reflection, eternalized configuration and bean cache

--- src/main/java/org/syh/dao/BeanFactory.java
+++ src/main/java/org/syh/dao/BeanFactory.java
@@ -1,9 +1,12 @@
 package org.syh.dao;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class BeanFactory {
     private static Properties properties;
+    private static Map<String, Object> beanMap = new HashMap<>();
 
     static {
         properties = new Properties();
@@ -15,14 +18,22 @@ public class BeanFactory {
     }
 
     public static Object getBean(String beanName) {
-        try {
-            Class<?> beanClass = Class.forName(properties.getProperty(beanName));
-            return beanClass.newInstance();
-        } catch (ClassNotFoundException exp) {
-            exp.printStackTrace();
-            throw new RuntimeException("BeanFactory have not [" + beanName + "] bean!", exp);
-        } catch (IllegalAccessException | InstantiationException exp) {
-            throw new RuntimeException("[" + beanName + "] instantiation error!", exp);
+        if (!beanMap.containsKey(beanName)) {
+            synchronized (BeanFactory.class) {
+                if (!beanMap.containsKey(beanName)) {
+                    try {
+                        Class<?> beanClass = Class.forName(properties.getProperty(beanName));
+                        Object bean = beanClass.newInstance();
+                        beanMap.put(beanName, bean);
+                    } catch (ClassNotFoundException exp) {
+                        exp.printStackTrace();
+                        throw new RuntimeException("BeanFactory have not [" + beanName + "] bean!", exp);
+                    } catch (IllegalAccessException | InstantiationException exp) {
+                        throw new RuntimeException("[" + beanName + "] instantiation error!", exp);
+                    }
+                }
+            }
         }
+        return beanMap.get(beanName);
     }
 }
